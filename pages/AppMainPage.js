de78f07212c3a33dbf137637ada9d773a74407de
tkdudsh <syn_1234@naver.com>
2025-05-13 01:00:15 +0900

    이름 나이 성별 추가

--- pages/AppMainPage.js
+++ pages/AppMainPage.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { TouchableOpacity,StyleSheet ,Text,View,Switch} from "react-native";
+import { TouchableOpacity,StyleSheet ,Text,View,Switch,TextInput} from "react-native";
 import { KeyboardAvoidingView, ScrollView } from "react-native";
-import{ useState } from "react"; // ✅ useState 추가
+import{ useState } from "react";
 
 export default function AppMainPage({navigation}) {
+
+    const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+
+
     const [DarkMode,setDarkMode]=useState(false);
+    const handleSubmit = () => {
+  console.log(`이름: ${name}, 나이: ${age}, 성별: ${gender}`);
+};
+
 
     const backgroundColor = DarkMode ? "#222" : "#F5F5F0";
     const textColor = DarkMode ? "#FFFFFF" : "#111111";
@@ -27,7 +37,37 @@ export default function AppMainPage({navigation}) {
                 <Text style={[styles.warningText, { color: textColor }]}>- 질문을 빠지지 마고 꼭 끝까지 들은 후 반응해주세요.</Text>
                 <Text style={[styles.warningText, { color: textColor }]}>- 검사 중 스마트폰은 움직이지 않도록 해주세요.</Text>
               </View>
-              <TouchableOpacity style={[styles.testButton, { backgroundColor: DarkMode ? "#5DADE2" : "#4A90E2" }]}  onPress={function(){navigation.navigate('Tutorial')}}>
+
+              <View style={styles.container}>
+      <Text style={styles.title}>사용자 정보 입력</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="이름"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="나이"
+        keyboardType="numeric"
+        value={age}
+        onChangeText={setAge}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="성별 (남 / 여)"
+        value={gender}
+        onChangeText={setGender}
+      />
+
+    </View>
+              <TouchableOpacity style={[styles.testButton, { backgroundColor: DarkMode ? "#5DADE2" : "#4A90E2" }]}
+               onPress={function(){
+                navigation.navigate('Tutorial'); 
+               handleSubmit();}}>
                   <Text style={styles.testButtonText}>
                       검사하기
                   </Text>
@@ -91,4 +131,7 @@ const styles = StyleSheet.create({
         top: 70,
         right: 16,
     },
+    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 30 },
+  input: { width: 250, height: 40, borderColor: '#ccc', borderWidth: 1, borderRadius: 8, marginBottom: 15, paddingHorizontal: 10 },
+  button: { backgroundColor: '#4A90E2', padding: 15, borderRadius: 10 },
 });
